18e4c73a24f792f75ebbbd3718879940455a837f
vvaibhavv11 <vvaibhavv3434@skiff.com>
2023-09-11 00:26:51 +0530

    drop idea
    idea of dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  darkMode = false;
+  constructor(){
+    this.dectectColorScheme();
+  }
+
+  dectectColorScheme(){
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+      this.darkMode = true;
+      document.documentElement.setAttribute('data_theme', this.darkMode ? 'dark' : 'Light');
+    }
+  }
+
+  Theme(){
+    this.darkMode =! this.darkMode;
+    document.documentElement.setAttribute('data_theme', this.darkMode ? 'dark' : 'light');
+  }
 }
